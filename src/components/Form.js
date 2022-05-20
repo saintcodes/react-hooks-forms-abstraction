3be7c584ac5e9395212c3c32ac90d7db90b6d272
Michelle <clairsin@gmail.com>
2022-05-20 10:10:34 -0400

    commit tree 13f24e8546865b85e3c747bc4c6c4b7dc3b9e1ac

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,72 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admine: false,
+  })
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
+
+  function handleChange(e) {
+    e.preventDefault()
+    const name = e.target.name
+    let value = e.target.value
+    
+    if (e.target.type === "checkbox") {
+      value = e.target.checked
+    }  
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+
+
+  }
+  console.log(formData)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(formData)
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+  // function handleFirstNameChange(e) {
+  //   setFormData({
+  //     ...formData,
+  //     firstName: e.target.value, 
+  //   })
+  // }
+
+  // function handleLastNameChange(e) {
+  //   setFormData({
+  //     ...formData,
+  //     lastName: e.target.value,
+  //   })
+  // }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        name="firstName"
+        value={formData.firstName} 
+        onChange={handleChange} 
+      />
+      <input 
+        type="text" 
+        name="lastName"
+        value={formData.lastName} 
+        onChange={handleChange} 
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        checked={formData.admin}
+        onChange={handleChange}
+        />
       <button type="submit">Submit</button>
     </form>
   );
